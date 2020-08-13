ebb92109b4724b9860a635362228c05b7a127770
Gayan <gayantrr@gmail.com>
2020-08-13 10:13:58 +0530

    Add project files.

---
+++ MyAddressBook/ContactValidation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyAddressBook
+{
+    public class ContactValidation
+    {
+        [Display(Name= "First Name")]
+        [Required(ErrorMessage = "Please provide First Name", AllowEmptyStrings = false)]
+        public string ContactPersonFname { get; set; }
+
+
+        [Display(Name= "Last Name")] // It is not required
+        public string ContactPersonLname { get; set; }
+
+        [Display(Name= "Contact No1")]
+        [Required(ErrorMessage = "Please provide First Name", AllowEmptyStrings = false)]
+        public string ContactNo1 { get; set; }
+
+        [Display(Name= "Contact No2")]
+        public string ContactNo2 { get; set; }
+
+        [Display(Name= "Email ID")]
+        [RegularExpression(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$", ErrorMessage = "Email not valid")]
+
+        public string EmailID { get; set; }
+
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Please Select Country")]
+        public int CountryID { get; set; }
+
+        [Display(Name = "State")]
+        [Required(ErrorMessage = "Please Select State")]
+        public int StateID { get; set; }
+    }
+
+    [MetadataType(typeof(ContactValidation))]   // Apply validation
+    public partial class Contact
+    {
+
+    }
+}
---
+++ MyAddressBook/Controllers/HomeController.cs
@@ -0,0 +1,157 @@
+﻿using MyAddressBook.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyAddressBook.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Get: /Home/
+        public ActionResult Index()
+        {
+            List<ContactModel> contacts = new List<ContactModel>();
+            using (MyAddressBookEntities dc = new MyAddressBookEntities())
+            {
+                var v = (from a in dc.Contacts
+                         join b in dc.Countries on a.CountryID equals b.CountryID
+                         join c in dc.States on a.StateID equals c.StateID
+                         select new ContactModel
+                         {
+                             ContactID = a.ContactID,
+                             FirstName = a.ContactPersonFname,
+                             LastName = a. ContactPersonLname,
+                             ContactNo1 = a.ContactNo1,
+                             ContactNo2 = a.ContactNo2,
+                             EmailID = a.EmailID,
+                             Country = b.CountryName,
+                             State = c.StateName,
+                             Address = a.Address,
+                             ImagePath = a.ImagePath
+                         }).ToList();
+                contacts = v;
+            }
+            return View(contacts);
+        }
+
+        public ActionResult Add()
+        {
+            //fetch country data
+            List<Country> AllCountry = new List<Country>();
+            List<State> states = new List<State>();
+            // Hre MyAddressBookEntities is our DbContext
+            using (MyAddressBookEntities dc = new MyAddressBookEntities())
+            {
+                AllCountry = dc.Countries.OrderBy(a => a.CountryName).ToList();
+                //Not need to fetch state as we dont know witch country user select here
+            }
+
+            ViewBag.Country = new SelectList(AllCountry, "CountryID", "CountryName");
+            ViewBag.State = new SelectList(states, "StateID", "StateName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Contact c, HttpPostedFileBase file)
+        {
+            #region // Fetch Country & state
+            List<Country> allCountry = new List<Country>();
+            List<State> states = new List<State>();
+            using (MyAddressBookEntities dc = new MyAddressBookEntities())
+            {
+                allCountry = dc.Countries.OrderBy(a => a.CountryName).ToList();
+                if(c.CountryID > 0)
+                {
+                    states = dc.States.Where(a => a.CountryID.Equals(c.CountryID)).OrderBy(a => a.StateName).ToList();
+                }
+            }
+            ViewBag.Country = new SelectList(allCountry, "CountryID", "CountryName", c.CountryID);
+            ViewBag.State = new SelectList(states, "StateID", "StateName", c.StateID);
+            #endregion
+            #region// Validation file if selected
+            if (file != null)
+            {
+                if(file.ContentLength>(512*100))    //512KB
+                {
+                    ModelState.AddModelError("FileErrorMessage", "File size must be withing 512KB");
+                }
+                string[] allowedType = new string[] { "image/png", "image/gif", "image/jpeg", "image/jpg" };
+                bool isFlileTypeValid = false;
+                foreach(var i in allowedType)
+                {
+                    if(file.ContentType == i.ToString())
+                    {
+                        isFlileTypeValid = true;
+                        break;
+                    }
+                }
+                if (!isFlileTypeValid)
+                {
+                    ModelState.AddModelError("FileErroeMessage", "Only .png, .gif, .jpg file type allowed");
+                }
+            }
+            #endregion
+            #region // Validate Model and Save to DB
+            if (ModelState.IsValid)
+            {
+                //save here
+                if(file != null)
+                {
+                    string savePath = Server.MapPath("~/image");
+                    string fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    file.SaveAs(Path.Combine(savePath, fileName));
+                    c.ImagePath = fileName;
+                }
+                using(MyAddressBookEntities dc = new MyAddressBookEntities())
+                {   
+                    dc.Contacts.Add(c);
+                    dc.SaveChanges(); 
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(c);
+            }
+            #endregion
+
+        }
+
+        // Action for fetch state
+        public JsonResult GetStates(int countryID)
+        {
+            using (MyAddressBookEntities dc = new MyAddressBookEntities())
+            {
+                // will off Lazy Loading
+                var State = (from a in dc.States
+                             where a.CountryID.Equals(countryID)
+                              orderby a.StateName
+                             select a).ToList();
+                return new JsonResult { Data = State, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        public ActionResult Export()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Your register page.";
+
+            return View();
+        }
+    }
+}
